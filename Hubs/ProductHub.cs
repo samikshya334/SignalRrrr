2319f722e570d800bc50b2e25ded1430ab3a274f
samikshya regmi <regmisamu0@gmail.com>
2024-03-27 13:09:13 +0545

    hello

---
+++ Hubs/ProductHub.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalR.Hubs
+{
+    public class ProductHub:Hub
+    {
+        public static int AvailableQuantity = 1;
+        public async Task PurchaseProduct()
+        {
+            if (AvailableQuantity>0)
+            {
+                AvailableQuantity--;
+                await Clients.Caller.SendAsync("ProductSuccess", "Pruchase Order success");
+                if(AvailableQuantity==0)
+                {
+                    await Clients.Others.SendAsync("ProductSoldOut", "Sorry no More product");
+                }
+
+            }
+            else
+            {
+                await Clients.Caller.SendAsync("PurchaseFailed", "Sorry no More product");
+            }
+        }
+    }
+}
+
---
+++ wwwroot/js/Product.js
@@ -0,0 +1,21 @@
+﻿const connection = new signalR.HubConnectionBuilder().withUrl("/checkproduct").build();
+connection.on("ProductSuccess", function (message) {
+    document.getElementById("message").textContent = message;
+    document.getElementById("productBtn").disabled = true;
+
+});
+connection.on("ProductSoldOut", function (message) {
+    document.getElementById("message").textContent = message;
+    document.getElementById("productBtn").disabled = true;
+
+});
+connection.on("PurchaseFailed", function (message) {
+    documnet.getElementById("message").textContent = message;
+    document.getElementById("productBtn").disabled = true;
+
+});
+connection.start();
+document.getElementById("productBtn").addEventListener("click",function()
+{
+    connection.invoke("PurchaseProduct").catch(err => console.error(err));
+})
